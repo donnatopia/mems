056eb2275e7f02ae55d6e3b99e824d244f6c685a
Donna <donnatopia@gmail.com>
2023-05-17 14:43:49 -0700

    set up project with react native, expo, and tailwindcss

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+      <Text>hello world</Text>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

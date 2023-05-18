69dab45a6d0038294f0aa05117cdd730682ce304
Donna <donnatopia@gmail.com>
2023-05-17 22:08:20 -0700

    created homescreen

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,17 @@ module.exports = {
   "./pages/**/*.{js,ts,jsx,tsx}",
   "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0097B2',
+        secondary: '#0CC0DF',
+        tertiary: '#5CE1E6',
+        dark: {
+          1: '#020617', // slate-950
+          2: '#1E293B', // slate-800
+        },
+      }
+    },
   },
   plugins: [],
 }
